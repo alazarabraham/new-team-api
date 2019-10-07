0a4d9a219d08354c2343fe3406c4a1859317486b
Alazar Markos <alazarabraham1@gmail.com>
2019-10-07 11:38:00 -0400

    ingredient switch statment

--- js.js
+++ js.js
@@ -3,24 +3,25 @@
 var counter = 0;
 var key = "";
 document.querySelector("#submitform").addEventListener("submit", getrecipe);
+
 function getrecipe(e) {
   const submit = document.querySelector(".submit").value;
   fetch(`https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=onions,garlic&q=${submit}&p=3`)
     .then(response => {
       if (response.status != 200) {
-          document.querySelector("#recipeList").innerHTML = "NO RESULTS";
-          console.log("Error");
+        document.querySelector("#recipeList").innerHTML = "NO RESULTS";
+        console.log("Error");
         throw error(response.statusText);
       } else {
         return response.json();
       }
     })
     .then(data => {
-      if(data.results.length == 0){
+      if (data.results.length == 0) {
         //tell the user no response
         //use innerHTML to tell the user no response
         console.log("NO RESULTS");
-      }else{
+      } else {
         createRecipeCard(data);
       }
       console.log(data);
@@ -28,7 +29,8 @@ function getrecipe(e) {
     .catch(err => console.log(err));
   e.preventDefault();
 }
-function createRecipeCard(data) { 
+
+function createRecipeCard(data) {
   //manipulate data
   let recipes = data.results;
   //loop through recipes
@@ -72,23 +74,52 @@ function createRecipeCard(data) {
       // if(ingredientsArray[j].toUpperCase() == "RED CHILLIES" || ingredientsArray[j].toUpperCase() == "JALAPENOS"){
       //   //show a red pepper for hot
       // }
-      //create ingredient to add to list
       const ingredient = document.createElement("li");
       ingredient.innerHTML = ingredientsArray[j];
       // getIngredientProperty(ingredientsArray[j]);
-      switch (ingredientsArray[j].toUpperCase()) {
-        case "CORNMEAL":
+      switch (ingredientsArray[j].toLowerCase()) {
+        case "bread":
+        case "cornmeal":
+        case " white rice":
+        case "oats":
+        case "potato":
+        case "sugar":
+        case "ice cream":
+        case "white bread":
+        case "rice":
 
-          ingredient.innerHTML += "<span style='color:brown;'>*</span>";
+          ingredient.innerHTML += "<notification style='background: #4900D8;'> <-This Ingredient has 'bad' carbs, if on a keto diet avoid this ingredient</notification>";
           //console.log("This has carbs");
           break;
-        case "RED PEPPER":
-            ingredient.innerHTML += "<span style='color:red;'>*</span>";
-          //tell people its spicy
+        case "lemon":
+        case "lemon juice":
+        case "limes":
+        case "lime":
+        case "lemons":
+        case "pomegranate":
+        case "grapefruit":
+        case "red pepper":
+        case "pineapple":
+        case "barbecue":
+        case "red chilies":
+        case "jalapeno":
+        case "tomato":
+        case "tomato sauce":
+
+          ingredient.innerHTML += "<notification style='background: #00D154;'> <-This Ingredient has a high PH level, avoid if your diet requires low acidic foods</notification>";
           break;
-        case "JALAPENOS":
-            ingredient.innerHTML += "<span style='color:red;'>*</span>";
-          //tell people its spicy
+        case "avocado":
+        case "apple":
+        case "oats":
+        case "blueberry":
+        case "strawberry":
+        case "peach":
+        case "prune":
+        case "broccali":
+        case "pear":
+
+
+          ingredient.innerHTML += "<notification style='background: #EB00D9;'> <-This ingredient is considered a good carb, and is good for your brain!</notification>";
           break;
       }
       ingredientsList.appendChild(ingredient);
@@ -106,8 +137,8 @@ function createRecipeCard(data) {
 // function getIngredientProperty(ingredient, ingredientElement){
 //   fetch("")
 //     .then(response => function(result){
-      //append result to ingredient
-      //ingredientElement.innerHTML += result.data;
-    // })
+//append result to ingredient
+//ingredientElement.innerHTML += result.data;
+// })
 
 // }
